2cc17dc1c551c805329103b38bdafce31a53e7fe
anveloper <hitedin@gmail.com>
2025-04-08 18:49:01 +0900

    chore: base 수정

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/product-detail-page/",
+  base: "/no-code-detail/",
   plugins: [react()],
   resolve: {
     alias: {
