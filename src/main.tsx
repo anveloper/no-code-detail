5a1ed76676fc5c0afac0bdf679b0edbe99f6529e
anveloper <hitedin@gmail.com>
2025-04-08 14:13:41 +0900

    feat: 기본 파일 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import "./global.css";
+
+import ProductDetail from "./product-detail";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ProductDetail />
   </StrictMode>
 );
